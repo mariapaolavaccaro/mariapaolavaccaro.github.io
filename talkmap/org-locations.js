d4ca05479f7a5ee4ba89614336e67c97af32d4af
mariapaolavaccaro <96374380+mariapaolavaccaro@users.noreply.github.com>
2025-06-24 15:35:12 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,5 +18,15 @@ var addressPoints = [
     "EAS annual meeting",
     45.407717, 
     11.873446
+  ],
+  [
+    " Massive Black Holes in the First Billion Years",
+    51.897928, 
+    -8.470581
+  ],
+  [
+    "RESCEU-NBIA workshop on GW sources",
+    35.682839, 
+    139.759455
   ]
 ];
